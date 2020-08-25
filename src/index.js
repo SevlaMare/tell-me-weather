6146fe001d840b6004e952335469ec4fd361ae18
Mare <sevilamare@gmail.com>
2020-08-25 16:49:28 -0300

    get input v1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-let api = '6ca8aaad293b0b0a3ad93f29e82425f8'
-let city = 'tokyo'
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}`
-
-function handleError(fx) {
-  return function (...params) {
-    return fx(...params).catch(
-      function (error) {
-        console.warn('Something goes wrong:', error)
-      }
-    )
-  }
-}
+const sel = (selector) => document.querySelector(selector);
+// let city = 'tokyo'
 
 async function fetchData(url) {
   const response = await fetch(url);
   const data = await response.json();
-  return { name: data.name, temp: data.main.temp };
+  return { name: data.name, temp: data.main.temp, weather: data.weather[0].main };
 }
 
-fetchData(url).then(console.log)
+// fetchData(url).then(console.log)
+sel('#btnSend').addEventListener('click', (Event) => {
+  // get city form input
+  const city = sel('#city').value
+  const api = '6ca8aaad293b0b0a3ad93f29e82425f8'
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}`
+
+  // display data
+  fetchData(url).then(console.log)
+
+  Event.preventDefault()
+})
